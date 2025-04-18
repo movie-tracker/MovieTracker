7b489fbed78f05cbab35a837d9a684cbf901178c
Felipe Kelemen Bernardino <felipe.kelemen02@gmail.com>
2025-04-18 16:09:16 -0300

    feat: add find user by email

--- internal/services/user.service.go
+++ internal/services/user.service.go
@@ -1,14 +1,17 @@
 package services
 
 import (
+	"github.com/go-jet/jet/v2/qrm"
 	"github.com/movie-tracker/MovieTracker/internal/repositories"
 	"github.com/movie-tracker/MovieTracker/internal/services/dto"
+	"github.com/movie-tracker/MovieTracker/internal/utils"
 )
 
 const COST = 14
 
 type IUserService interface {
 	FindAll() ([]dto.UserDTO, error)
+	FindByEmail(email string) (dto.UserDTO, error)
 	Create(dto.UserCreateDTO) (dto.UserDTO, error)
 }
 
@@ -42,6 +45,24 @@ func (s UserService) FindAll() ([]dto.UserDTO, error) {
 	}
 
 	return userDTOs, err
+}
+
+func (s UserService) FindByEmail(email string) (dto.UserDTO, error) {
+	var err error
+	var userDTO dto.UserDTO
+
+	user, err := s.userRepo.FindByEmail(email)
+	if err != nil {
+		switch err {
+		case qrm.ErrNoRows:
+			return userDTO, utils.NewNotFoundError("error.user.not_found")
+		default:
+			return userDTO, err
+		}
+	}
+
+	userDTO.FromModel(user)
+	return userDTO, err
 }
 
 func (s UserService) Create(userDTO dto.UserCreateDTO) (createdUserDTO dto.UserDTO, err error) {
---
+++ internal/utils/errors.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+	"net/http"
+)
+
+type IApiError interface {
+	Error() string
+	BuildError() (int, any)
+}
+
+type ApiError struct {
+	Message string `json:"message"`
+	Code    int    `json:"status_code"`
+}
+
+func (e *ApiError) Error() string {
+	return e.Message
+}
+
+func (e *ApiError) BuildError() (int, any) {
+	return e.Code, e
+}
+
+type InternalServerError struct {
+	ApiError
+	Err error `json:"-"`
+}
+
+func NewInternalServerError(err error) *InternalServerError {
+	return &InternalServerError{
+		ApiError: ApiError{
+			Message: "error.internal_server_error",
+			Code:    http.StatusInternalServerError,
+		},
+		Err: err,
+	}
+}
+
+func NewNotFoundError(message string) *ApiError {
+	return &ApiError{
+		Message: FallbackZero(message, "error.not_found"),
+		Code:    http.StatusNotFound,
+	}
+}
