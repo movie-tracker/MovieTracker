6970f2b0c83b1edd15f4a2c407956c9b8f494e48
Felipe Kelemen Bernardino <felipe.kelemen02@gmail.com>
2025-04-18 00:24:14 -0300

    feat: add users table

---
+++ internal/migrations/20250418030837_add_user_table.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+-- +goose StatementBegin
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  username VARCHAR UNIQUE NOT NULL,
+  phone VARCHAR UNIQUE NOT NULL,
+  email VARCHAR UNIQUE NOT NULL,
+  password VARCHAR NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP
+);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+DROP TABLE users;
+
+-- +goose StatementEnd
