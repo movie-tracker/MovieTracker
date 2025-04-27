12c5377db8e8fac4490e8c7ba5629a41d7246845
Felipe Kelemen Bernardino <felipe.kelemen02@gmail.com>
2025-04-27 17:53:50 -0300

    feat: added jwt validation

--- internal/controllers/controllers.go
+++ internal/controllers/controllers.go
@@ -1,12 +1,9 @@
 package controllers
 
 import (
-	"log/slog"
-
 	"github.com/gin-gonic/gin"
 	"github.com/movie-tracker/MovieTracker/internal/config"
 	"github.com/movie-tracker/MovieTracker/internal/services"
-	"github.com/movie-tracker/MovieTracker/internal/utils"
 )
 
 type IController interface {
@@ -41,23 +38,4 @@ func (c *Controllers) Register(router gin.IRouter, prefix string) {
 
 func path(prefix string, path string) string {
 	return prefix + path
-}
-
-func MakeHandler(f func(*gin.Context) error) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		err := f(c)
-
-		if err == nil {
-			return
-		}
-
-		if apiErr, ok := err.(utils.IApiError); ok {
-			statusCode, response := apiErr.BuildError()
-			c.JSON(statusCode, response)
-			return
-		}
-
-		slog.Error("error handling request", "error", err)
-		c.AbortWithError(500, err)
-	}
 }
