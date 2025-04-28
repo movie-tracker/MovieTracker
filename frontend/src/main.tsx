7d6f6f42fe0985c61167aa4c3adf58dc5c2fc600
Felipe Kelemen Bernardino <felipe.kelemen02@gmail.com>
2025-04-28 13:48:20 -0300

    feat: add i18n & login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import AppConfig, { AppStage } from "./config/index.ts";
+import App from "@/App.tsx";
+import AppConfig, { AppStage } from "@/config/index.ts";
+
+import "@/config/i18n.ts";
+import "@/index.css";
 
 const root = createRoot(document.getElementById("root")!);
 
--- frontend/src/utils/errors.ts
+++ frontend/src/utils/errors.ts
@@ -1,4 +1,14 @@
-export interface IApiError {
+import { TFunction } from "i18next";
+
+export interface IApiError extends Error {
   message: string;
   statusCode: number;
+}
+
+export function getErrorMessage(error: Error, t: TFunction) {
+  if (error.name === "API_ERROR") {
+    const apiError = error as IApiError;
+    return t(`api.${apiError.statusCode}`, { message: apiError.message });
+  }
+  return error.message;
 }
