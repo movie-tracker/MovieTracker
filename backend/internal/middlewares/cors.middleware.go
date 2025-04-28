b922a01d8310c181101378d01d47aa78a8ad7469
Felipe Kelemen Bernardino <felipe.kelemen02@gmail.com>
2025-04-28 08:50:48 -0300

    feat: add cors headers

---
+++ backend/internal/middlewares/cors.middleware.go
@@ -0,0 +1,28 @@
+package middlewares
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/movie-tracker/MovieTracker/internal/config"
+)
+
+func CORSMiddleware(cfg config.ApiConfig) gin.HandlerFunc {
+	allowOrigin := cfg.AllowOrigin
+	return func(c *gin.Context) {
+
+		fmt.Printf("CORS: %s\n", allowOrigin)
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
