cee1021d0290533e8137ffaa6349b271a99d72ac
Felipe Kelemen Bernardino <felipe.kelemen02@gmail.com>
2025-04-27 23:07:28 -0300

    feat: strict mode for development

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import AppConfig, { AppStage } from "./config/index.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root")!);
+
+if (AppConfig.stage === AppStage.DEVELOPMENT) {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  root.render(<App />);
+}
