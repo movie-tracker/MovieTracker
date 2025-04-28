b410a2232d25879611303e776d48f10184379ae3
Felipe Kelemen Bernardino <felipe.kelemen02@gmail.com>
2025-04-27 23:08:40 -0300

    feat: add auth context

---
+++ frontend/src/context/AuthContext/auth.context.ts
@@ -0,0 +1,15 @@
+import { UseQueryResult } from "@tanstack/react-query";
+import UserDTO from "@/services/dto/user.dto";
+import { createContext } from "react";
+
+export interface IAuthContext {
+  user?: UserDTO;
+  profileQuery: UseQueryResult<UserDTO, Error>;
+  isAuthenticated: boolean;
+  login: (username: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+const AuthContext = createContext<IAuthContext>({} as IAuthContext);
+
+export default AuthContext;
