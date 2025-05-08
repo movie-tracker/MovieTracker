bd4de780b2f0e161236e2bc6044d57e1d12a4360
KANE1 <taqtile@Corona.local>
2025-05-08 00:20:46 -0300

    chore: setup dashboard route

---
+++ frontend/src/pages/Dashboard/routes.tsx
@@ -0,0 +1,8 @@
+import { RouteObject } from 'react-router-dom';
+
+import DashboardPage from './Dashboard';
+
+export default {
+  path: '/dashboard',
+  element: <DashboardPage />,
+} as RouteObject;
